3880d6823f6c96b51d55ade250abdeb51c2c9662
Ritesh Pal <riteshpal861@gmail.com>
2026-09-15 00:32:35 +0530

    Create day3.java
    declared a variable and data type

---
+++ day3.java
@@ -0,0 +1,18 @@
+// Variable and data type in java 
+pubic class day3{
+  pubic static void main(String [] args){
+    // Declare a Variable in java 
+    string Name = "Ritesh Pal"
+    int age = 22;
+    float persent =85.6;
+    bool pass = True;
+    system.out.printn(" Your name :- "+Name);
+    system.out.printn("The age is :- "+age);
+    system.out.printn("Persent are :- "+persent);
+     system.out.printn("Are you in a  :- "+pass);
+    // there are 2 type of data type in java programming 
+    // 1. primitive 2. non- primitive
+    // Primitive Data Type - int ,float,bool,string,double,char
+    // Non- Primitive - array, list,etc
+  }
+}
