ce77d541d8898590e8bc7cab49c6369f0bda90a8
Ritesh Pal <riteshpal861@gmail.com>
2026-09-14 15:53:04 +0530

    Create day2.java
    new change

---
+++ day2.java
@@ -0,0 +1,11 @@
+// Comment in Java Programming
+public class day2{
+  public static void main(String [] args){
+    // There are 2 Type of comment in Java Language
+    //1. Single Line Comment
+   /*
+     Multiline Comment in java 
+    */
+    // Java Compiler Ignore these Syntax.
+  }
+}
