b0f56738dfc981ba133d5d462e70e87040268bc6
Ritesh Pal <riteshpal861@gmail.com>
2026-09-14 15:42:19 +0530

    Create day1.java
    coding change in day 1

---
+++ day1.java
@@ -0,0 +1,7 @@
+// Frist Program in Java Language
+public class day1{
+  public static void main(String [] args){
+    system.out.println("Hii Java Developer's");
+  }
+}
+// OutPut :- Hii Java Developer's 
