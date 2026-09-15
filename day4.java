d1c0965a834f1b607e184ba1d7d5a077f0d96473
Ritesh Pal <riteshpal861@gmail.com>
2026-09-16 02:04:41 +0530

    Create day4.java
    Input and outputput code include in java

---
+++ day4.java
@@ -0,0 +1,18 @@
+// Take input from the user 
+import java.util.*;
+      public class day4{
+        public static void main(String [] args){
+          Scanner sc = new Scanner(System.in);
+          system.out.println("Enter your name : - ");
+          String name = sc.nextLine();
+          system.out.println("Your name is : - "+name);
+          system.out.println("Enter your age :- ");
+          int age = Sc.nextInt();
+          system.out.println("Your age is :- "+age);
+          /*
+          Output on this code :
+          Your name is : - Ritesh Pal
+          Your age is :- 21
+          */
+        }
+      }
