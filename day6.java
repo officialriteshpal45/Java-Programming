2d329f79c19311e6d30748b5c6a78860639468e7
Officialritesh45 <riteshpal861@gmail.com>
2026-09-16 16:39:49 +0530

    day6.java

---
+++ day6.java
@@ -0,0 +1,18 @@
+public class day6 {
+    public static void main(String[] args) {
+        // assignment operator in java
+        int a = 10; 
+        System.out.println("Value of a: " + a);
+        System.out.println("Value of a after assignment: " + a);
+        a += 5; 
+        System.out.println("Value of a after addition assignment: " + a);
+        a -= 3;
+        System.out.println("Value of a after subtraction assignment: " + a);
+        a *= 2;
+        System.out.println("Value of a after multiplication assignment: " + a);
+        a /= 4;
+        System.out.println("Value of a after division assignment: " + a);
+        a %= 3;
+        System.out.println("Value of a after modulus assignment: " + a);
+    }
+}
---
+++ day7.java
@@ -0,0 +1,12 @@
+public class day7 {
+    public static void main(String[] args) {
+        // logical operator in java
+        boolean pass = true;
+        boolean fail = false;
+        System.out.println("Your are pass in exam: " + pass);
+        System.out.println("Your are fail in exam: " + fail);
+        System.out.println("pass && fail: " + (pass && fail));
+        System.out.println("pass || fail: " + (pass || fail));
+        System.out.println("!pass: " + !pass);
+    }
+}
